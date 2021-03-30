6b873712cfcadca83d21cf74f5c34f00d9884e77
Marley Boone <mboone30@ivyhtech.edu>
2021-03-30 09:48:56 -0400

    Added Category Class  and Cahnged and Updated Identity Models

---
+++ To-Gather.Data/Category.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace To_Gather.Data
+{
+    public class Category
+    {
+        [Key]
+        public int CategoryId { get; set; }
+        public Guid OwnerId { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        [MaxLength(100, ErrorMessage ="Description cannot be over 100 characters")]
+        public string Description { get; set; }
+    }
+}
