74746701de7c3e0c3d44c684e6a7b879947a0c8a
Marley Boone <mboone30@ivyhtech.edu>
2021-03-30 13:44:52 -0400

    Changed connection string also able to run app

--- To-Gather.WebMVC/Controllers/AccountController.cs
+++ To-Gather.WebMVC/Controllers/AccountController.cs
@@ -8,6 +8,7 @@ using System.Web.Mvc;
 using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.Owin;
 using Microsoft.Owin.Security;
+using To_Gather.Data;
 using To_Gather.WebMVC.Models;
 
 namespace To_Gather.WebMVC.Controllers
